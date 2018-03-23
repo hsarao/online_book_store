9fcc0f209017c3554a2b33ae044b701df74ac79b
Jasleen Sran <jslnsran@gmail.com>
2018-03-23 14:24:47 -0500

    Added image column to books table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323174257) do
+ActiveRecord::Schema.define(version: 20180323190811) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20180323174257) do
     t.integer "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["genre_id"], name: "index_books_on_genre_id"
   end
 
