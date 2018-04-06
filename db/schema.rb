758657d50cb0e286c9ea485e9e80e741b1fdd38d
Jasleen Sran <jslnsran@gmail.com>
2018-04-05 19:41:35 -0500

    Added Sale and new Column to books Table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406003055) do
+ActiveRecord::Schema.define(version: 20180406004045) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 20180406003055) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.decimal "price"
+    t.boolean "sale"
+    t.boolean "new"
     t.index ["genre_id"], name: "index_books_on_genre_id"
   end
 
